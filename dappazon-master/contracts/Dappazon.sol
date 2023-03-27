e8eaf97db525a8ce6bac0efe726a5f39a2d8860f
Kashyapriya <95755693+Kashyapriya@users.noreply.github.com>
2023-03-27 11:46:28 +0530

    Update Dappazon.sol

--- dappazon-master/contracts/Dappazon.sol
+++ dappazon-master/contracts/Dappazon.sol
@@ -94,85 +94,96 @@ contract Dappazon {
 
 
 
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
-// Components
-import Navigation from './components/Navigation'
-import Section from './components/Section'
-import Product from './components/Product'
+contract Dappazon {
+    address public owner;
 
-// ABIs
-import Dappazon from './abis/Dappazon.json'
-
-// Config
-import config from './config.json'
-
-function App() {
-  const [provider, setProvider] = useState(null)
-  const [dappazon, setDappazon] = useState(null)
-
-  const [account, setAccount] = useState(null)
-
-  const [electronics, setElectronics] = useState(null)
-  const [clothing, setClothing] = useState(null)
-  const [toys, setToys] = useState(null)
-
-  const [item, setItem] = useState({})
-  const [toggle, setToggle] = useState(false)
-
-  const togglePop = (item) => {
-    setItem(item)
-    toggle ? setToggle(false) : setToggle(true)
-  }
-
-  const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
-    const network = await provider.getNetwork()
-
-    const dappazon = new ethers.Contract(config[network.chainId].dappazon.address, Dappazon, provider)
-    setDappazon(dappazon)
-
-    const items = []
-
-    for (var i = 0; i < 9; i++) {
-      const item = await dappazon.items(i + 1)
-      items.push(item)
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
     }
 
-    const electronics = items.filter((item) => item.category === 'electronics')
-    const clothing = items.filter((item) => item.category === 'clothing')
-    const toys = items.filter((item) => item.category === 'toys')
+    struct Order {
+        uint256 time;
+        Item item;
+    }
 
-    setElectronics(electronics)
-    setClothing(clothing)
-    setToys(toys)
-  }
+    mapping(uint256 => Item) public items;
+    mapping(address => mapping(uint256 => Order)) public orders;
+    mapping(address => uint256) public orderCount;
 
-  useEffect(() => {
-    loadBlockchainData()
-  }, [])
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
+    event List(string name, uint256 cost, uint256 quantity);
 
-  return (
-    <div>
-      <Navigation account={account} setAccount={setAccount} />
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
-      <h2>Dappazon Best Sellers</h2>
+    constructor() {
+        owner = msg.sender;
+    }
 
-      {electronics && clothing && toys && (
-        <>
-          <Section title={"Clothing & Jewelry"} items={clothing} togglePop={togglePop} />
-          <Section title={"Electronics & Gadgets"} items={electronics} togglePop={togglePop} />
-          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
-        </>
-      )}
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public onlyOwner {
+        // Create Item
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
 
-      {toggle && (
-        <Product item={item} provider={provider} account={account} dappazon={dappazon} togglePop={togglePop} />
-      )}
-    </div>
-  );
+        // Add Item to mapping
+        items[_id] = item;
+
+        // Emit event
+        emit List(_name, _cost, _stock);
+    }
+
+    function buy(uint256 _id) public payable {
+        // Fetch item
+        Item memory item = items[_id];
+
+        // Require enough ether to buy item
+        require(msg.value >= item.cost);
+
+        // Require item is in stock
+        require(item.stock > 0);
+
+        // Create order
+        Order memory order = Order(block.timestamp, item);
+
+        // Add order for user
+        orderCount[msg.sender]++; // <-- Order ID
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        // Subtract stock
+        items[_id].stock = item.stock - 1;
+
+        // Emit event
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
+    }
 }
-
-export default App;
